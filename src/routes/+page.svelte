ef2decb1909c973c7ca6713abbf8a32b859c19cd
Vinayak Tyagi <vinayak@Vinayaks-MacBook-Air.local>
2024-03-28 21:29:52 +0530

    Hooked the editor with the Rope

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,46 @@
 <script>
 	import { RopeNode } from '../lib/rope.js';
 	let rope = new RopeNode('');
+	let editor;
 	function handleKeyDown(event) {
-		const cursorPosition = parseInt(event.selectionStart);
-		rope.insert(cursorPosition, event.key);
-	}
-	function walkRope(event) {
-		rope.walk();
+		const cursorPosition = parseInt(editor.selectionStart);
+		switch (event.key) {
+			case 'ArrowLeft':
+				editor.selectionStart = editor.selectionEnd = Math.max(0, editor.selectionStart - 1);
+				return;
+			case 'ArrowRight':
+				editor.selectionStart = editor.selectionEnd = Math.min(
+					editor.selectionStart + 1,
+					editor.value.length
+				);
+				break;
+			case 'Backspace':
+				if (cursorPosition > 0) {
+					rope.remove(cursorPosition - 1, cursorPosition);
+					editor.selectionStart = editor.selectionEnd = Math.max(0, editor.selectionStart - 1);
+				}
+				break;
+			case 'CapsLock':
+				break;
+			default:
+				rope.insert(cursorPosition, event.key);
+		}
+		editor.value = rope.getLeaves();
 	}
 </script>
 
-<h1 class="flex w-full justify-center text-2xl text-gray-800 p-4">Welcome to my code editor</h1>
-<div class="flex flex-col">
-	<div class="flex flex-col h-80 p-4">
-		<textarea on:keydown={handleKeyDown} class="flex grow p-4 border border-gray-800"></textarea>
+<body class="bg-slate-800 w-full h-screen">
+	<h1 class="flex w-full justify-center text-2xl text-white font-mono p-4">
+		Rope based editor in JS
+	</h1>
+	<div class="flex flex-col">
+		<div class="flex flex-col h-80 p-4 mx-5">
+			<textarea
+				id="editor"
+				on:keydown|preventDefault={handleKeyDown}
+				bind:this={editor}
+				class="flex text-white grow p-4 rounded-lg border border-gray-800 font-mono bg-gray-900 focus:outline-gray-600"
+			></textarea>
+		</div>
 	</div>
-	<button on:click={walkRope} class="p-4 bg-transparent border border-black">Walk the rope</button>
-</div>
+</body>
